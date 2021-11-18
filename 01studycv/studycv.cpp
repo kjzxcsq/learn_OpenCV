a2b6f2869d6822c403ad50e9244c038f15962dee
kjzxcsq <1114764192@qq.com>
2021-11-18 13:00:11 +0800

    learn ROI (deep)

--- 01studycv/studycv.cpp
+++ 01studycv/studycv.cpp
@@ -1,3 +1,4 @@
+#include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
@@ -10,7 +11,7 @@ int main(int, char**)
 {
     Mat frame;
     cout << "Opening camera..." << endl;
-    VideoCapture capture(0); // open the first camera
+    VideoCapture capture(2); // open the first camera
     if (!capture.isOpened())
     {
         cerr << "ERROR: Can't initialize camera capture" << endl;
@@ -25,8 +26,27 @@ int main(int, char**)
             cerr << "ERROR: Can't grab camera frame." << endl;
             break;
         }
+        resize(frame, frame, Size(1080, 720), 0, 0, INTER_AREA);
         
-        imshow("Frame", frame);
+        Mat frame_copy, frame_deepcopy, frame_deepcopy2;
+        frame_copy = frame;
+        frame.copyTo(frame_deepcopy);
+        copyTo(frame, frame_deepcopy2, frame);
+
+        Rect rect(200, 200, 400, 400);
+        Mat ROI1, ROI2, ROI_deepcopy;
+        ROI1 = frame(rect);
+        ROI2 = frame(Range(100, 500), Range(200, 400));
+        frame(Range(100, 500), Range(200, 400)).copyTo(ROI_deepcopy);
+        circle(frame, Point(300, 300), 200, Scalar(0, 0, 255), -1);
+
+        imshow("frame", frame);
+        imshow("frame_copy", frame_copy);
+        imshow("frame_deepcopy", frame_deepcopy);
+        imshow("frame_deepcopy2", frame_deepcopy2);
+        imshow("ROI1", ROI1);
+        imshow("ROI2", ROI2);
+        imshow("ROI_deepcopy", ROI_deepcopy);
 
         int key = waitKey(1);
         if (key == 27/*ESC*/)
@@ -34,4 +54,3 @@ int main(int, char**)
     }
     return 0;
 }
-
