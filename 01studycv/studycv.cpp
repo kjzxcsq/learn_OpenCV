9b4ba88b9b7730ce26cc49b9e7641d6214902007
kjzxcsq <1114764192@qq.com>
2021-11-23 03:38:15 +0800

    learn filter

--- 01studycv/studycv.cpp
+++ 01studycv/studycv.cpp
@@ -25,8 +25,25 @@ int main(int, char**)
             cerr << "ERROR: Can't grab camera frame." << endl;
             break;
         }
+        resize(frame, frame, Size(), 0.5, 0.5);
+        Mat mean_filter_3, mean_filter_9;
+        blur(frame, mean_filter_3, Size(3, 3));
+        blur(frame, mean_filter_9, Size(9, 9));
         
         imshow("Frame", frame);
+        imshow("mean_filter_3", mean_filter_3);
+        imshow("mean_filter_9", mean_filter_9);
+
+        Mat bilateral1, bilateral2, bilateral3, bilateral4;
+        bilateralFilter(frame, bilateral1, 5, 50, 25 / 2);
+        bilateralFilter(frame, bilateral2, 25, 50, 25 / 2);
+        bilateralFilter(frame, bilateral3, 5, 9, 9);
+        bilateralFilter(frame, bilateral4, 5, 200, 200);
+
+        imshow("bilateral1", bilateral1);
+        imshow("bilateral2", bilateral2);
+        imshow("bilateral3", bilateral3);
+        imshow("bilateral4", bilateral4);
 
         int key = waitKey(1);
         if (key == 27/*ESC*/)
