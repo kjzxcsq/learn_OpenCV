be186100f4a759d5263742576743ff69d6867de9
kjzxcsq <1114764192@qq.com>
2021-11-25 20:18:15 +0800

    learn morphology

--- 01studycv/studycv.cpp
+++ 01studycv/studycv.cpp
@@ -25,8 +25,40 @@ int main(int, char**)
             cerr << "ERROR: Can't grab camera frame." << endl;
             break;
         }
+        resize(frame, frame, Size(), 0.5, 0.5);
+        imshow("frame", frame);
+        cvtColor(frame, frame, COLOR_BGR2GRAY);
+        imshow("gray", frame);
+        threshold(frame, frame, 100, 255, THRESH_BINARY);
+        imshow("thresh", frame);
         
-        imshow("Frame", frame);
+        //5*5矩形结构元素
+        Mat kernel = getStructuringElement(0, Size(5, 5));
+        Mat open, close, gradient, tophat, blackhat, hitmiss;
+
+        //开运算
+        morphologyEx(frame, open, MORPH_OPEN, kernel);
+        imshow("open", open);
+
+        //闭运算
+        morphologyEx(frame, close, MORPH_CLOSE, kernel);
+        imshow("close", close);
+
+        //形态学梯度运算
+        morphologyEx(frame, gradient, MORPH_GRADIENT, kernel);
+        imshow("gradient", gradient);
+
+        //顶帽运算
+        morphologyEx(frame, tophat, MORPH_TOPHAT, kernel);
+        imshow("tophat", tophat);
+
+        //黑帽运算
+        morphologyEx(frame, blackhat, MORPH_BLACKHAT, kernel);
+        imshow("blackhat", blackhat);
+
+        //击中击不中变换
+        morphologyEx(frame, hitmiss, MORPH_HITMISS, kernel);
+        imshow("hitmiss", hitmiss);
 
         int key = waitKey(1);
         if (key == 27/*ESC*/)
