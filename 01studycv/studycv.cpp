c3b32786f3ad89d7b4f094a8a5f52c3363c88154
kjzxcsq <1114764192@qq.com>
2021-11-29 12:45:57 +0800

    learn houghlinesp

--- 01studycv/studycv.cpp
+++ 01studycv/studycv.cpp
@@ -17,6 +17,14 @@ int main(int, char**)
         return 1;
     }
 
+    int line_threshold = 150;           //累加器阈值
+    int minlength = 30;                 //直线最小长度
+    int maxgap = 10;                    //两点间最大距离
+    namedWindow("find_lines", WINDOW_NORMAL);
+    createTrackbar("line_threshold", "find_lines", &line_threshold, 500);
+    createTrackbar("minlength", "find_lines", &minlength, 300);
+    createTrackbar("maxgap", "find_lines", &maxgap, 100);
+
     for (;;)
     {
         capture >> frame; // read the next frame from camera
@@ -26,7 +34,23 @@ int main(int, char**)
             break;
         }
         resize(frame, frame, Size(), 0.5, 0.5);
-        imshow("frame", frame);
+
+        Mat edge;
+        //检测边缘并二值化
+        Canny(frame, edge, 80, 180);
+        threshold(edge, edge, 170, 255, THRESH_BINARY);
+        imshow("edge", edge);
+
+        //渐进概率式霍夫变换提取直线
+        std::vector<Vec4i> linesP;
+        HoughLinesP(edge, linesP, 1, CV_PI / 180, line_threshold, minlength, maxgap);
+
+        //绘制直线
+        for(size_t i = 0; i < linesP.size(); i++)
+        {
+            line(frame, Point(linesP[i][0], linesP[i][1]), Point(linesP[i][2], linesP[i][3]), Scalar(255), 3);
+        }
+        imshow("find_lines", frame);
 
         int key = waitKey(1);
         if (key == 27/*ESC*/)
